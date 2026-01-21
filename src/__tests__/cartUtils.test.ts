46987970ee102d6cf8e5718533445048791d6725
kaiwelch <158845766+kaiwelch@users.noreply.github.com>
2026-01-20 20:41:01 -0800

    a

--- src/__tests__/cartUtils.test.ts
+++ src/__tests__/cartUtils.test.ts
@@ -4,7 +4,7 @@ import {
 	calculateTax,
 	calculateTotal,
 	CartItem,
-} from "../cartUtils";
+} from "../cartUtils.ts";
 
 describe("applyDiscount", () => {
 	it("applies a percentage discount to a price", () => {
